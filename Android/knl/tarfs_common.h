7f8bf5859e17485c3579b5112b3863f67490f028
tmori <tmori@devAndroid.(none)>
2010-06-21 23:56:05 +0900

    ASSERT add

--- Android/knl/tarfs_common.h
+++ Android/knl/tarfs_common.h
@@ -12,27 +12,25 @@
 #endif
 #define ASSERT(EXP)						\
 	if (!(EXP))	{ printk("ASSERTION FAILED:%s:%d:%s\n", \
-			__FUNCTION__, __LINE__, #EXP); }
+			__FUNCTION__, __LINE__, #EXP); panic(#EXP);}
 /*
- * I/O 要求用のデータ
- * (要求できるI/O 個数は１個だけで、サイズは 512byte)
  */
 struct tarfs_io;
 struct tarfs_io_operation;
 struct tarfs_io {
-	uint64_t io_blkno; 	/* IN:512 byte 単位 */
-	void*	 io_data;	/* OUT: I/O 完了したデータ */
-	void*	 io_bh;		/* OS 依存データ(linux の場合は buffer_head) */
-	void*	 io_dev;	/* OS 依存データ(linux の場合は super_block) */
+	uint64_t io_blkno; 	/*  */
+	void*	 io_data;	/*  */
+	void*	 io_bh;		/*  */
+	void*	 io_dev;	/*  */
 	const struct tarfs_io_operation* oper;
 };
 typedef struct tarfs_io tarfs_io_t;
 
 struct tarfs_io_operation {
 	int32_t	 (*init) (void* , tarfs_io_t *);
-	int32_t	 (*bget) (struct tarfs_io *, uint64_t); /* OS 依存関数(getblk)に相当する */
-	int32_t	 (*bread) (struct tarfs_io *, uint64_t); /* OS 依存関数(sb_bread)に相当する */
-	int32_t	 (*bput) (struct tarfs_io *); /* OS 依存関数(brelse)に相当する */
+	int32_t	 (*bget) (struct tarfs_io *, uint64_t); /*  */
+	int32_t	 (*bread) (struct tarfs_io *, uint64_t); /*  */
+	int32_t	 (*bput) (struct tarfs_io *); /*  */
 };
 typedef struct tarfs_io_operation tarfs_io_operation_t;
 extern const tarfs_io_operation_t tarfs_io_oper;
