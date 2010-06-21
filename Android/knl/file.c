62d23c2dac2dc23327af682cf3c7cd068a5782e1
tmori <tmori@devAndroid.(none)>
2010-06-22 01:15:44 +0900

    add debug func

--- Android/knl/file.c
+++ Android/knl/file.c
@@ -1,10 +1,17 @@
 #include "tarfs_common_lnx.h"
 
+static ssize_t tarfs_sync_read(struct file *filp, char __user *buf, size_t len, loff_t *ppos);
+
 struct file_operations tarfs_file_operations = {
 	llseek:		generic_file_llseek,
-	read:		do_sync_read,
+	read:		tarfs_sync_read,
 	aio_read:	generic_file_aio_read,
 	mmap:		generic_file_mmap,
 	open:		generic_file_open,
 };
 
+// for debug
+static ssize_t tarfs_sync_read(struct file *filp, char __user *buf, size_t len, loff_t *ppos)
+{
+	return do_sync_read(filp, buf, len, ppos);
+}
