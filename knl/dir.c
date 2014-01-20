f4685947622e97f905b3df976dff0a9c545123d8
Hiroo MATSUMOTO <hiroom2.mail@gmail.com>
2014-01-20 19:39:02 +0900

    Convert readdir to iterate
    2233f31aade393641f0eaed43a71110e629bb900 in
    kernel/git/torvalds/linux.git.

--- knl/dir.c
+++ knl/dir.c
@@ -1,10 +1,18 @@
 #include "tarfs_common_lnx.h"
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,11,0)
 static int tarfs_readdir(struct file * filp, void * dirent, filldir_t filldir);
+#else
+static int tarfs_iterate(struct file * filp, struct dir_context *ctx);
+#endif
 
 struct file_operations tarfs_dir_operations = {
 	read:		generic_read_dir,	/* just put error */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,11,0)
 	readdir:	tarfs_readdir,
+#else
+	iterate:        tarfs_iterate,
+#endif
 };
 
 static int getDtype(int type)
@@ -21,6 +29,7 @@ static int getDtype(int type)
 	}
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,11,0)
 static int
 tarfs_readdir(struct file * filp, void * dirent, filldir_t filldir)
 {
@@ -77,4 +86,55 @@ errdone:
 	//printk("tarfs_readdir:ret=%d\n", err);
 	return err;
 }
+#else
+static int tarfs_iterate(struct file * filp, struct dir_context *ctx)
+{
+	int err = 0;
+	struct inode *ip;
+	tarfs_dinode_t *parentp = NULL;
+	tarfs_direct_t *dep = NULL;
+	uint64_t off;
+	uint64_t blkno;
+	uint64_t endoff;
+	tarfs_io_t io;
 
+	ip = filp->f_dentry->d_inode;
+	parentp = (tarfs_dinode_t *)ip->i_private;
+	endoff = parentp->di_size;
+	off = ctx->pos & ~(TAR_BLOCKSIZE - 1);
+	tarfs_io_oper.init(ip->i_sb, &io);
+
+	while (off < endoff) {
+		if ((off & (TAR_BLOCKSIZE -1)) == 0) { // 512 align
+			if (io.io_data != NULL) {
+				io.oper->bput(&io);
+			}
+			err = tarfs_getDataBlock(&io, parentp, off/TAR_BLOCKSIZE, &blkno);
+			if (err != 0) {
+				goto errdone;
+			}
+			err = io.oper->bread(&io, blkno);
+			if (err != 0) {
+				goto errdone;
+			}
+			dep = (tarfs_direct_t*)io.io_data;
+		}
+		if (off >= ctx->pos && dep->d_ino != TARBLK_NONE) {
+			if (!dir_emit(ctx, dep->d_name, dep->d_namelen,
+				      dep->d_ino, getDtype(dep->d_ftype))) {
+				goto errdone;
+			}
+		}
+		if (off >= ctx->pos) {
+			ctx->pos += dep->d_reclen;
+		}
+		off += dep->d_reclen;
+		dep = (tarfs_direct_t*)(((char*)dep) + dep->d_reclen);
+	}
+errdone: 
+	if (io.io_data != NULL) {
+		io.oper->bput(&io);
+	}
+	return err;
+}
+#endif
